85fabc449b4640433565e6dc5083c196f1d1f5e6
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-13 19:25:24 -0300

    Criando o DTO NewsDto e UserDto

---
+++ src/main/java/com/santander_dev_week/dto/NewsDto.java
@@ -0,0 +1,18 @@
+package com.santander_dev_week.dto;
+
+import com.santander_dev_week.domain.model.News;
+
+public record NewsDto(Long id, String icon, String description) {
+
+    public NewsDto(News model) {
+        this(model.getId(), model.getIcon(), model.getDescription());
+    }
+
+    public News toModel() {
+        News model = new News();
+        model.setId(this.id);
+        model.setIcon(this.icon);
+        model.setDescription(this.description);
+        return model;
+    }
+}
